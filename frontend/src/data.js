c8fde4b06aaa8b133fa30fc1d9004675a53c3674
Justin John Paul P <justinjjp006@gmail.com>
2022-03-17 14:56:42 +0530

    Share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Race Car',
+            category: 'Art Work',
+            image: '/images/p1.jpg',
+            price: 120,
+            author: 'vincy',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality images',
+
+        },
+        {
+            _id:'2',
+            name: 'Montel ',
+            category: 'Art Work',
+            image: '/images/p2.jpg',
+            price: 120,
+            author: 'JjPp',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality images',
+
+        },
+        {
+            _id:'3',
+            name: 'Skulls',
+            category: 'Art Work',
+            image: '/images/p3.jpg',
+            price: 100,
+            author: 'John',
+            rating: 4,
+            numReviews: 10,
+            description: 'High quality images',
+
+        },
+        {
+            _id:'4',
+            name: 'DJ Wolf ',
+            category: 'Art Work',
+            image: '/images/p4.jpg',
+            price: 120,
+            author: 'JjPp',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality images',
+
+        },
+        {
+            _id:'5',
+            name: 'Motercycle Life',
+            category: 'Art Work',
+            image: '/images/p5.jpg',
+            price: 130,
+            author: 'Jusstin',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High quality images',
+
+        },
+        {
+            _id:'6',
+            name: 'Dodge challanger',
+            category: 'Art Work',
+            image: '/images/p6.jpg',
+            price: 150,
+            author: 'Justin',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'High quality images',
+
+        },
+
+    ],
+};
+export default data;
